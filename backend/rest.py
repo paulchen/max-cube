b2a9412f2e25dfc4538d01e68bf9267354f923f5
Paul Staroch <paulchen@rueckgr.at>
2018-09-02 21:03:19 +0200

    refactored rest.py to use @app.route

--- backend/rest.py
+++ backend/rest.py
@@ -3,28 +3,33 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from threading import Thread
 import database
 
+app = Flask(__name__)
+# TODO configurable origins
+CORS(app, origins="*", allow_headers=[
+    "Content-Type", "Authorization", "Access-Control-Allow-Credentials"])
+
+update_callback = None
+
+@app.route('/max/rooms')
+def get_all_rooms():
+    rooms = database.get_all_rooms()
+    return jsonify({'rooms': rooms})
+
+
+@app.route('/max/update')
+def update_cube():
+    thread = Thread(target = update_callback)
+    thread.start()
+    return jsonify({'status': 'ok'})
+
+
 def run_api(execute_api_update):
-    app = Flask(__name__)
-    api = Api(app)
-    # TODO configurable origins
-    CORS(app, origins="*", allow_headers=[
-            "Content-Type", "Authorization", "Access-Control-Allow-Credentials"])
+    global update_callback, app
 
-
-    class RoomApi(Resource):
-        def get(self):
-            rooms = database.get_all_rooms()
-            return jsonify({'rooms': rooms})
-
-    class UpdateApi(Resource):
-        def get(self):
-            execute_api_update()
-            return jsonify({'status': 'ok'})
-
-    api.add_resource(RoomApi, '/max/rooms')
-    api.add_resource(UpdateApi, '/max/update')
+    update_callback = execute_api_update
 
     # TODO configurable port
     app.run(port=5003)
