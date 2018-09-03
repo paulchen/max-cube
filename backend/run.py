8ee28704cc5da19daa041e289460dce9c5ed8527
Paul Staroch <paulchen@rueckgr.at>
2018-09-03 19:55:06 +0200

    use try-finally to improve error handling

--- backend/run.py
+++ backend/run.py
@@ -14,23 +14,34 @@ t = None
 
 lock = Lock()
 
+def touch(path):
+    with open(path, 'a'):
+        os.utime(path, None)
+
+
 def update_data():
     global t
     print('abc')
     lock.acquire()
-    fetch.update_cube(settings)
-    lock.release()
-    print('def')
-    # TODO configurable time
-    t = Timer(300.0, update_data)
-    t.start()
+    try:
+        fetch.update_cube(settings)
+        print('def')
+        # TODO configurable file
+        touch('last_cube_update')
+    finally:
+        lock.release()
+        # TODO configurable time
+        t = Timer(300.0, update_data)
+        t.start()
 
 
 def room_update(room_id, temperature):
     print(temperature)
     lock.acquire()
-    fetch.update_room(settings, room_id, temperature)
-    lock.release()
+    try:
+        fetch.update_room(settings, room_id, temperature)
+    finally:
+        lock.release()
 
 
 def execute_api_update():
