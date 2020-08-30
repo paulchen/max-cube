0c2f94cee9b4f38cc6c29b2f7a2ee299a7a25c1a
Paul Staroch <paulchen@rueckgr.at>
2020-08-30 23:47:57 +0200

    use WatchedFileHandler to make external log rotation possible

--- backend/log.py
+++ backend/log.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 import os, logging
+from logging.handlers import WatchedFileHandler
 
 path = os.path.dirname(os.path.abspath(__file__)) + '/'
 
 logfile = path + 'log/max.log'
 
 logger = logging.getLogger()
-handler = logging.FileHandler(logfile)
+handler = WatchedFileHandler(logfile)
 handler.setFormatter(logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s'))
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
